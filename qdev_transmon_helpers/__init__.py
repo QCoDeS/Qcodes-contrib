5e258f5f613e35bac2fd6fca02e1b69727569cf0
Natalie Pearson <npearson@phys.ethz.ch>
2017-03-23 17:03:09 +0100

    tweak import in init

--- qdev_transmon_helpers/__init__.py
+++ qdev_transmon_helpers/__init__.py
@@ -1,3 +1,8 @@
+pulse_building_folder = '/c/Users/qcodes-natalie/PulseBuilding'
+if pulse_building_folder not in sys.path:
+    sys.path.insert(0, pulse_building_folder)
+from pulse_building import Waveform, Element, Sequence
+
 from .math_functions import *
 from .file_functions import *
 from .calib_dict_functions import *
@@ -7,8 +12,4 @@ from .vna_helper_functions import *
 from .alazar_rs_helper_functions import *
 from .instr_import_functions import *
 from .awg_helper_functions import *
-
-pulse_building_folder = '/c/Users/qcodes-natalie/PulseBuilding'
-if pulse_building_folder not in sys.path:
-    sys.path.insert(0, pulse_building_folder)
-from pulse_building import Waveform, Element, Sequence
+from .alazar_awg_wrapper import *
